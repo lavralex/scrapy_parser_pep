1aaadd9fd424e2fc5ba3ab26abf2f5030b7196cd
Александр Лаврушенков <lavralex2015@yandex.ru>
2023-12-16 23:13:34 +0300

    пройдены тесты

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,43 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import csv
+import datetime as dt
+from collections import Counter
 
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+from .settings import (
+    BASE_DIR, ENCODING,
+    RESULT_DIR,
+    DATETIME_FORMAT,
+    STATUSES_FILE_NAME,
+    FILE_FORMAT,
+)
 
 
 class PepParsePipeline:
+
+    def open_spider(self, spider):
+        self.statuses_summary = Counter()
+
     def process_item(self, item, spider):
+        self.statuses_summary[item['status']] += 1
         return item
+
+    def close_spider(self, spider):
+        creation_date = dt.datetime.now().strftime(DATETIME_FORMAT)
+        file_name = f'{STATUSES_FILE_NAME}_{creation_date}.{FILE_FORMAT}'
+        with open(
+            BASE_DIR / RESULT_DIR / file_name,
+            'w',
+            encoding=ENCODING
+        ) as file:
+            writer = csv.writer(
+                file,
+                dialect='unix',
+                quoting=csv.QUOTE_NONE,
+                escapechar='|'
+            )
+            writer.writerows(
+                (
+                    ('status', 'Count'),
+                    *self.statuses_summary.items(),
+                    ('Total', sum(self.statuses_summary.values()))
+                )
+            )
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -3,12 +3,13 @@ import re
 import scrapy
 
 from pep_parse.items import PepParseItem
+from pep_parse.settings import DOMAIN, SPIDER_NAME
 
 
 class PepSpider(scrapy.Spider):
-    name = 'pep'
-    allowed_domains = ['peps.python.org']
-    start_urls = ['https://peps.python.org/']
+    name = SPIDER_NAME
+    allowed_domains = [DOMAIN]
+    start_urls = [f'https://{DOMAIN}/']
 
     def parse(self, response):
         links = response.xpath(
@@ -18,12 +19,16 @@ class PepSpider(scrapy.Spider):
             yield response.follow(link, callback=self.parse_pep)
 
     def parse_pep(self, response):
-        title = response.xpath('//h1[contains(@class,"page-title")]/text()').get()
-        pattern = re.compile(r'^PEP\s(?P<number>\d+)\s–\s(?P<name>.*)')
+        title = response.xpath(
+            '//h1[contains(@class,"page-title")]/text()'
+        ).get()
+        pattern = re.compile(
+            r'^PEP\s(?P<number>\d+)\s–\s(?P<name>.*)'
+        )
         title_information = pattern.search(title)
         yield PepParseItem({
             'number': title_information.group('number'),
-            'name': title_information.group('name').strip().replace('\n', '').replace('\r', ''),
+            'name': title_information.group('name').strip(),
             'status': response.xpath(
                 '//dt[contains(text(),"Status")]/'
                 'following-sibling::dd/abbr/text()'
